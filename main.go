534fdb92f46eecda89566969ae405b0b56846df8
myleo1 <504861978@qq.com>
2021-01-29 13:45:16 +0800

    update fix:post info and bugs

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"Auto-NCO-ZJGSU/service"
-	"github.com/mizuki1412/go-core-kit/class/exception"
+	"Auto-NCO-ZJGSU/cmd"
 	"github.com/mizuki1412/go-core-kit/init/initkit"
-	"github.com/mizuki1412/go-core-kit/service/configkit"
-	"github.com/spf13/cast"
-	"time"
 )
 
 func main() {
 	initkit.LoadConfig()
-	users, err := cast.ToSliceE(configkit.Get("id", ""))
-	if err != nil {
-		panic(exception.New(err.Error()))
-	}
-	for _, v := range users {
-		user := cast.ToStringMapString(v)
-		cookie := service.GetCookie(user["name"], user["psswd"], user["userAgent"], user["home"])
-		service.Push2WeChat(user["wechatPushKey"], user["name"], user["trueName"], service.PostInfo(cookie, user["userAgent"]))
-		time.Sleep(time.Second * 1)
-	}
+	cmd.Execute()
 }
